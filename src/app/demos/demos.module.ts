984ee88109c15b51c8962e7bdf68e562db06b6ca
Hemant Singh <hemant.ajax@gmail.com>
2021-04-13 12:37:28 +0530

    demos setup

--- src/app/demos/demos.module.ts
+++ src/app/demos/demos.module.ts
@@ -3,15 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { DemosRoutingModule } from './demos-routing.module';
 import { DemosComponent } from './demos.component';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    DemosComponent
-  ],
+  declarations: [DemosComponent, ToolbarComponent],
   imports: [
     CommonModule,
-    DemosRoutingModule
-  ]
+    DemosRoutingModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+  ],
 })
-export class DemosModule { }
+export class DemosModule {}
