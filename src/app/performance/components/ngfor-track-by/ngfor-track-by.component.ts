fc206a684fb46d6b24694a0e8e3ee7bd52083e2b
Hemant <hksinghgl@gmail.com>
2021-04-29 19:28:12 +0530

    Performance - ngFor solution - trackBy

--- src/app/performance/components/ngfor-track-by/ngfor-track-by.component.ts
+++ src/app/performance/components/ngfor-track-by/ngfor-track-by.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 const EXAMPLE_DATA: any[] = [
   {
@@ -31,6 +31,7 @@ const EXAMPLE_DATA: any[] = [
   selector: 'app-ngfor-track-by',
   templateUrl: './ngfor-track-by.component.html',
   styleUrls: ['./ngfor-track-by.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NgforTrackByComponent implements OnInit {
   records = [];
@@ -54,11 +55,39 @@ export class NgforTrackByComponent implements OnInit {
         weight: 1.0079,
         symbol: 'Hemant',
       },
-      ...EXAMPLE_DATA,
+      {
+        weight: 1.0079,
+        symbol: 'H',
+      },
+      {
+        weight: 4.0026,
+        symbol: 'He',
+      },
+      {
+        weight: 6.941,
+        symbol: 'Li',
+      },
+      {
+        weight: 9.0122,
+        symbol: 'Be',
+      },
+      {
+        weight: 10.811,
+        symbol: 'B',
+      },
+      {
+        weight: 12.0107,
+        symbol: 'C',
+      },
     ];
   }
 
   resetList() {
     this.records = EXAMPLE_DATA;
+  }
+
+  symbolTrackBy(index, item) {
+    console.log(item);
+    return item.symbol;
   }
 }
