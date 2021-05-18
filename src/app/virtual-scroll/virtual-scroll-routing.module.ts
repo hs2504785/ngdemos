1d43a968f2e358259afde6cfa131adb757c0ee67
Hemant Singh <hemantsingh@Hemants-MacBook-Air.local>
2021-05-18 09:52:23 +0530

    chid route setup - virtual scroll

--- src/app/virtual-scroll/virtual-scroll-routing.module.ts
+++ src/app/virtual-scroll/virtual-scroll-routing.module.ts
@@ -1,9 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasicComponent } from './components/basic/basic.component';
+import { DemoComponent } from './components/demo/demo.component';
 import { VirtualScrollComponent } from './virtual-scroll.component';
 
-const routes: Routes = [{ path: '', component: VirtualScrollComponent }];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: VirtualScrollComponent,
+    children: [
+      {
+        path: 'basic',
+        component: BasicComponent,
+        pathMatch: 'full',
+      },
+      {
+        path: 'demo',
+        component: DemoComponent,
+        pathMatch: 'full',
+      },
+      {
+        path: '',
+        redirectTo: 'basic',
+      }
+    ],
+  },
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
--- src/app/virtual-scroll/virtual-scroll.module.ts
+++ src/app/virtual-scroll/virtual-scroll.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { VirtualScrollRoutingModule } from './virtual-scroll-routing.module';
 import { VirtualScrollComponent } from './virtual-scroll.component';
+import { BasicComponent } from './components/basic/basic.component';
+import { DemoComponent } from './components/demo/demo.component';
 
 
 @NgModule({
   declarations: [
-    VirtualScrollComponent
+    VirtualScrollComponent,
+    BasicComponent,
+    DemoComponent
   ],
   imports: [
     CommonModule,
