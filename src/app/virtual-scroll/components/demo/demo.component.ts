9bcc6169c33beb5f86673d63d17d673e69747e59
Hemant Singh <hemantsingh@Hemants-MacBook-Air.local>
2021-05-18 18:22:19 +0530

    photos virtual scroll - no api pagination

--- src/app/virtual-scroll/components/demo/demo.component.ts
+++ src/app/virtual-scroll/components/demo/demo.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { DataService } from 'src/app/shared/services/data.service';
 
 @Component({
   selector: 'app-demo',
@@ -6,10 +9,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demo.component.scss']
 })
 export class DemoComponent implements OnInit {
+  photos$: Observable<any[]>;
+  @ViewChild(CdkVirtualScrollViewport)
+  viewport: CdkVirtualScrollViewport;
 
-  constructor() { }
+  batch = 20;
+  theEnd = false;
 
-  ngOnInit(): void {
+  offset = new BehaviorSubject(null);
+  infinite: Observable<any[]>;
+
+  constructor(private dataService: DataService) {
+    // const batchMap = this.offset.pipe(
+    //   throttleTime(500),
+    //   mergeMap(n => this.getBatch(n)),
+    //   scan((acc, batch) => {
+    //     return { ...acc, ...batch };
+    //   }, {})
+    // );
+
+    // this.infinite = batchMap.pipe(map(v => Object.values(v)));
   }
 
+  ngOnInit() {
+    this.photos$ = this.dataService.getPhotos();
+  }
+
+  getBatch(offset) {
+    console.log(offset);
+    // return this.db
+    //   .collection('people', ref =>
+    //     ref
+    //       .orderBy('name')
+    //       .startAfter(offset)
+    //       .limit(this.batch)
+    //   )
+    //   .snapshotChanges()
+    //   .pipe(
+    //     tap(arr => (arr.length ? null : (this.theEnd = true))),
+    //     map(arr => {
+    //       return arr.reduce((acc, cur) => {
+    //         const id = cur.payload.doc.id;
+    //         const data = cur.payload.doc.data();
+    //         return { ...acc, [id]: data };
+    //       }, {});
+    //     })
+    //   );
+  }
+
+  nextBatch(e, offset) {
+    // if (this.theEnd) {
+    //   return;
+    // }
+
+    // const end = this.viewport.getRenderedRange().end;
+    // const total = this.viewport.getDataLength();
+    // console.log(`${end}, '>=', ${total}`);
+    // if (end === total) {
+    //   this.offset.next(offset);
+    // }
+  }
+
+  trackByIdx(i) {
+    return i;
+  }
 }
