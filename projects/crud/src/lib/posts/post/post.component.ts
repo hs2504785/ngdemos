0263988ef07d29b6e7d17b2959b25072a8a877ae
Hemant Singh <hemantsingh@Hemants-MacBook-Air.local>
2021-05-29 18:00:15 +0530

    post crud screen with add/edit form

--- projects/crud/src/lib/posts/post/post.component.ts
+++ projects/crud/src/lib/posts/post/post.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { PostInterface } from '../models/post.interface';
 
 @Component({
   selector: 'lib-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PostComponent implements OnInit {
+  post$: Observable<PostInterface> = of({
+    userId: 1,
+    id: 1,
+    title:
+      'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+  });
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
