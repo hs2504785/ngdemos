42861d829b1eceb79057df2e28974ee6c7200fa6
Hemant Singh <hemantsingh@Hemants-MacBook-Air.local>
2021-05-30 17:19:39 +0530

    CRUD NgRx Data - Get Single User

--- projects/crud/src/lib/users/user/user.component.ts
+++ projects/crud/src/lib/users/user/user.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserInterface } from '../models/user.interface';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'lib-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  user$: Observable<UserInterface>;
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+  ) {}
 
   ngOnInit(): void {
+    const currentUserId = this.route.snapshot.paramMap.get('id');
+    this.user$ = this.userService.getUserByIdFromCache(currentUserId);
   }
-
 }
