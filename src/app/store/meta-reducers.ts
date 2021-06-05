357267ab91939e160aad3fc796779358674ca401
Hemant Singh <hemantsingh@Hemants-MacBook-Air.local>
2021-06-05 06:14:37 +0530

    logger reducer updated

--- src/app/store/meta-reducers.ts
+++ src/app/store/meta-reducers.ts
@@ -4,16 +4,22 @@ import { AppState } from './reducers';
 
 export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
   return function newReducer(state, action) {
-    console.group(action.type);
     const nextState = reducer(state, action);
-    console.log(`%c prev state`, `color: #9E9E9E; font-weight: bold`, state);
-    console.log(`%c action`, `color: #03A9F4; font-weight: bold`, action);
-    console.log(
-      `%c next state`,
-      `color: #4CAF50; font-weight: bold`,
-      nextState,
-    );
-    console.groupEnd();
+
+    if (
+      !action.type.includes('router-store') &&
+      !action.type.includes('store-devtools')
+    ) {
+      console.group(action.type);
+      console.log(`%c prev state`, `color: #9E9E9E; font-weight: bold`, state);
+      console.log(`%c action`, `color: #03A9F4; font-weight: bold`, action);
+      console.log(
+        `%c next state`,
+        `color: #4CAF50; font-weight: bold`,
+        nextState,
+      );
+      console.groupEnd();
+    }
     return nextState;
   };
 }
