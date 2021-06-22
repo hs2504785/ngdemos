71d3c965b6f0491b5c1a77127bdc0b34ac9dab74
Hemant Singh <hemantsingh@Hemants-MacBook-Air.local>
2021-06-22 19:52:56 +0530

    style added fitness app

--- projects/fitness/src/app/app.module.ts
+++ projects/fitness/src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MatButtonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
