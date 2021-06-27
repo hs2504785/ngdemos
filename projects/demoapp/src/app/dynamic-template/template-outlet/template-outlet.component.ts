0011462f138cf7432ffc803f12d8c7bb2e2ab711
Hemant Singh <hemantsingh@Hemants-MacBook-Air.local>
2021-06-27 15:57:01 +0530

    ngTemplateOutletContext

--- projects/demoapp/src/app/dynamic-template/template-outlet/template-outlet.component.ts
+++ projects/demoapp/src/app/dynamic-template/template-outlet/template-outlet.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-template-outlet',
   templateUrl: './template-outlet.component.html',
-  styleUrls: ['./template-outlet.component.scss']
+  styleUrls: ['./template-outlet.component.scss'],
 })
-export class TemplateOutletComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TemplateOutletComponent {
+  ctx = {
+    $implicit: 'Arya Singh',
+    location: 'Ameenpur, India',
+  };
 }
