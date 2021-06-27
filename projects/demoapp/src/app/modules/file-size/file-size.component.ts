9dc9e71d4bc9bc1262b854718d576767ab48c3fb
Hemant Singh <hemantsingh@Hemants-MacBook-Air.local>
2021-06-27 20:13:36 +0530

    file size pipe from component

--- projects/demoapp/src/app/modules/file-size/file-size.component.ts
+++ projects/demoapp/src/app/modules/file-size/file-size.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FileSizePipe } from 'projects/libs/shared/src/public-api';
 
 interface File {
   name: string;
-  size: number;
+  size: any;
   type: string;
 }
 
@@ -10,14 +11,25 @@ interface File {
   selector: 'app-file-size',
   templateUrl: './file-size.component.html',
   styleUrls: ['./file-size.component.scss'],
+  providers: [FileSizePipe],
 })
 export class FileSizeComponent implements OnInit {
   files: File[];
+  mappedFiles: File[];
+  constructor(private fileSize: FileSizePipe) {}
+
   ngOnInit() {
     this.files = [
       { name: 'logo.svg', size: 2120109, type: 'image/svg' },
       { name: 'banner.jpg', size: 18029, type: 'image/jpg' },
       { name: 'background.png', size: 1784562, type: 'image/png' },
     ];
+
+    this.mappedFiles = this.files.map(file => {
+      return {
+        ...file,
+        size: this.fileSize.transform(file.size, 'mb'),
+      };
+    });
   }
 }
