42ccc72b81aa7edf6105f1734429e1951960c83b
Hemant <hemant.ajax@gmail.com>
2021-07-30 10:26:30 +0530

    intersection observer v0

--- src/app/modules/intersection-observer/image-demo/image-demo.component.ts
+++ src/app/modules/intersection-observer/image-demo/image-demo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IntersectionStatus } from 'src/app/shared/directives/from-intersection-observer';
 import { DataService } from 'src/app/shared/services/data.service';
 
 @Component({
@@ -9,10 +10,20 @@ import { DataService } from 'src/app/shared/services/data.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ImageDemoComponent implements OnInit {
+  visibilityStatus: { [key: string]: IntersectionStatus } = {};
+  intersectionStatus = IntersectionStatus;
   photos$: Observable<any[]>;
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.photos$ = this.dataService.getPhotos();
+  }
+
+  onVisibilityChanged(index: string, status: IntersectionStatus) {
+    this.visibilityStatus[index] = status;
+  }
+
+  trackByIndex(index: number, item) {
+    return index;
   }
 }
