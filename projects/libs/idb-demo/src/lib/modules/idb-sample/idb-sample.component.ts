7731b6f8424de21281ed765777f85af488a0c6e5
Hemant <hemant.ajax@gmail.com>
2021-08-07 09:29:43 +0530

    idb demo added

--- projects/libs/idb-demo/src/lib/modules/idb-sample/idb-sample.component.ts
+++ projects/libs/idb-demo/src/lib/modules/idb-sample/idb-sample.component.ts
@@ -1,15 +1,117 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxIndexedDBService } from 'ngx-indexed-db';
+import { forkJoin } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'lib-idb-sample',
   templateUrl: './idb-sample.component.html',
-  styleUrls: ['./idb-sample.component.css']
+  styleUrls: ['./idb-sample.component.css'],
 })
-export class IdbSampleComponent implements OnInit {
+export class IdbSampleComponent {
+  title = 'playground';
+  storeName: string;
 
-  constructor() { }
+  constructor(private dbService: NgxIndexedDBService) {}
 
-  ngOnInit(): void {
+  add(): void {
+    this.dbService
+      .add('people', {
+        name: `charles number ${Math.random() * 10}`,
+        email: `email number ${Math.random() * 10}`,
+      })
+      .subscribe(result => {
+        console.log('result: ', result);
+      });
   }
 
+  bulkAdd(): void {
+    this.dbService
+      .bulkAdd('people', [
+        {
+          name: `charles number ${Math.random() * 10}`,
+          email: `email number ${Math.random() * 10}`,
+        },
+        {
+          name: `charles number ${Math.random() * 10}`,
+          email: `email number ${Math.random() * 10}`,
+        },
+      ])
+      .subscribe(result => {
+        console.log('result: ', result);
+      });
+  }
+
+  bulkGet(): void {
+    for (let i = 0; i < 3; i++) {
+      this.bulkAdd();
+    }
+    this.dbService.bulkGet('people', [30, 34, 5]).subscribe(result => {
+      console.log('results: ', result);
+    });
+  }
+
+  update(): void {
+    this.dbService
+      .update('people', { id: 1, email: 'asd', name: 'charles' })
+      .subscribe(result => {
+        console.log('result: ', result);
+      });
+  }
+
+  delete(): void {
+    this.dbService.delete('people', 3).subscribe(result => {
+      console.log('result: ', result);
+    });
+  }
+
+  clean(): void {
+    this.dbService.clear('people').subscribe(result => {
+      console.log('result: ', result);
+    });
+  }
+
+  count(): void {
+    this.dbService.count('people').subscribe(result => {
+      console.log('result: ', result);
+    });
+  }
+
+  createStote(storeName: string): void {
+    console.log('storeName', storeName);
+    const storeSchema = {
+      store: storeName,
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [
+        { name: 'name', keypath: 'name', options: { unique: false } },
+        { name: 'email', keypath: 'email', options: { unique: false } },
+      ],
+    };
+
+    this.dbService.createObjectStore(storeSchema);
+  }
+
+  addTwoAndGetAllByIndex(): void {
+    // #209 getAllByIndex with multiple result should resolve observable
+    forkJoin([
+      this.dbService.add('people', {
+        name: `desmond`,
+        email: `email number ${Math.random() * 10}`,
+      }),
+      this.dbService.add('people', {
+        name: `desmond`,
+        email: `email number ${Math.random() * 10}`,
+      }),
+    ])
+      .pipe(
+        switchMap(() =>
+          this.dbService.getAllByIndex(
+            'people',
+            'name',
+            IDBKeyRange.only('desmond'),
+          ),
+        ),
+      )
+      .subscribe(result => console.log(result));
+  }
 }
--- projects/libs/idb-demo/src/lib/modules/idb-sample/idb-sample.module.ts
+++ projects/libs/idb-demo/src/lib/modules/idb-sample/idb-sample.module.ts
@@ -2,19 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { IdbSampleComponent } from './idb-sample.component';
+import { DBConfig, NgxIndexedDBModule } from 'ngx-indexed-db';
+import { FormsModule } from '@angular/forms';
 
+const dbConfig: DBConfig = {
+  name: 'MyDb',
+  version: 1,
+  objectStoresMeta: [
+    {
+      store: 'people',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [
+        { name: 'name', keypath: 'name', options: { unique: false } },
+        { name: 'email', keypath: 'email', options: { unique: false } },
+      ],
+    },
+  ],
+};
 
-const routes: Routes = [
-  { path: '', component: IdbSampleComponent }
-];
+const routes: Routes = [{ path: '', component: IdbSampleComponent }];
 
 @NgModule({
-  declarations: [
-    IdbSampleComponent
-  ],
+  declarations: [IdbSampleComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
-  ]
+    FormsModule,
+    RouterModule.forChild(routes),
+    NgxIndexedDBModule.forRoot(dbConfig),
+  ],
 })
-export class IdbSampleModule { }
+export class IdbSampleModule {}
