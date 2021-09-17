a7f82d83cb73f89a2c0fda959a889cb31f5380c3
Hemant <hemant.ajax@gmail.com>
2021-09-17 16:10:13 +0530

    linkedin post

--- projects/demoapp/src/app/modules/ng-challange/modules/components/components.module.ts
+++ projects/demoapp/src/app/modules/ng-challange/modules/components/components.module.ts
@@ -6,9 +6,15 @@ import { ComponentsComponent } from './components.component';
 import { SharedModule } from '../../shared/shared.module';
 import { LoaderComponent } from './loader/loader.component';
 import { TwitterPostComponent } from './twitter-post/twitter-post.component';
+import { LinkedinPostComponent } from './linkedin-post/linkedin-post.component';
 
 @NgModule({
-  declarations: [ComponentsComponent, LoaderComponent, TwitterPostComponent],
+  declarations: [
+    ComponentsComponent,
+    LoaderComponent,
+    TwitterPostComponent,
+    LinkedinPostComponent,
+  ],
   imports: [CommonModule, SharedModule, ComponentsRoutingModule],
 })
 export class ComponentsModule {}
--- projects/demoapp/src/app/modules/ng-challange/modules/components/linkedin-post/linkedin-post.component.ts
+++ projects/demoapp/src/app/modules/ng-challange/modules/components/linkedin-post/linkedin-post.component.ts
@@ -4,16 +4,14 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-linkedin-post',
   templateUrl: './linkedin-post.component.html',
-  styleUrls: ['./linkedin-post.component.scss']
+  styleUrls: ['./linkedin-post.component.scss'],
 })
 export class LinkedinPostComponent {
-
-  constructor(public router: Router) { }
+  constructor(public router: Router) {}
 
   public get linkedinMessage(): string {
     const base = 'https://www.linkedin.com/shareArticle?mini=true';
-    const currentSite = `&url=https://www.DylanIsrael.com${this.router.url}`;
-
+    const currentSite = `&url=https://hs2504785.github.io/ngdemos/ngapp&title=${this.router.url}`;
     return `${base}${currentSite}`;
   }
 }
