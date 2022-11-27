7bd3d18068830bbb54fc1047652c9eeb16427c98
Hemant Singh <hemantsingh@Hemants-MacBook-Air.local>
2022-11-27 20:05:55 +0530

    noscam demo - refactoring

--- src/app/modules/scam/modules/noscamdemo/modules/zippy/zippy.module.ts
+++ src/app/modules/scam/modules/noscamdemo/modules/zippy/zippy.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ZippyRoutingModule } from './zippy-routing.module';
 import { ZippyComponent as LandingZippy } from './zippy.component';
-import { UiModule } from '../../ui/ui.module';
+import { UiModule } from '../../../ui/ui.module';
 import { NoscamSharedModule } from '../../../shared/noscam-shared.module';
 import { ZippyComponent } from '../components/zippy/zippy.component';
 
--- src/app/modules/scam/modules/noscamdemo/modules/zippy1/zippy1.module.ts
+++ src/app/modules/scam/modules/noscamdemo/modules/zippy1/zippy1.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { Zippy1RoutingModule } from './zippy1-routing.module';
 import { Zippy1Component } from './zippy1.component';
-import { UiModule } from '../../ui/ui.module';
+import { UiModule } from '../../../ui/ui.module';
 import { NoscamSharedModule } from '../../../shared/noscam-shared.module';
 
 @NgModule({
