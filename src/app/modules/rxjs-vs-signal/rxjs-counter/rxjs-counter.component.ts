c3c6260a7582276b0087b4b41b57f7c4fcdc47dc
Hemant <hemant@Hemants-MacBook-Air.local>
2023-07-22 12:12:37 +0530

    rxjs counter done

--- src/app/modules/rxjs-vs-signal/rxjs-counter/rxjs-counter.component.ts
+++ src/app/modules/rxjs-vs-signal/rxjs-counter/rxjs-counter.component.ts
@@ -1,5 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RxjsCounterService } from '../services/rxjs-counter.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs-counter',
@@ -7,8 +14,19 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './rxjs-counter.component.html',
   styleUrls: ['./rxjs-counter.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RxjsCounterComponent {
+  private sub: Subscription;
+  private rxjsCounterService = inject(RxjsCounterService);
+  count$ = this.rxjsCounterService.count$;
+  double$ = this.rxjsCounterService.double$;
+  triple$ = this.rxjsCounterService.triple$;
+  combined$ = this.rxjsCounterService.combined$;
+  over9000$ = this.rxjsCounterService.over9000$;
+  message$ = this.rxjsCounterService.message$;
 
+  increment() {
+    this.rxjsCounterService.increment();
+  }
 }
---
+++ src/app/modules/rxjs-vs-signal/services/rxjs-counter.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, combineLatest, map } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class RxjsCounterService {
+  count$ = new BehaviorSubject(1000);
+  increment() {
+    this.count$.next(this.count$.value + 1);
+  }
+
+  double$ = this.count$.pipe(
+    map(count => {
+      console.log('Double: ', count);
+
+      return count * 2;
+    }),
+  );
+  triple$ = this.count$.pipe(
+    map(count => {
+      console.log('Triple: ', count);
+
+      return count * 3;
+    }),
+  );
+  combined$ = combineLatest([this.double$, this.triple$]).pipe(
+    map(([double, triple]) => {
+      console.log('Combined: ', double * triple);
+
+      return double * triple;
+    }),
+  );
+  over9000$ = this.combined$.pipe(
+    map(combined => {
+      console.log('Over9000: ', combined > 9000);
+      return combined > 9000;
+    }),
+  );
+  message$ = this.over9000$.pipe(
+    map(over9000 => {
+      const msg = over9000 ? `its over 9000` : 'its under 9000';
+      console.log('Message ', msg);
+      return msg;
+    }),
+  );
+}
